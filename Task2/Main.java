ceed2d2164dcf120fe1b27cff7a65f19d02f5cbd
weybsonalves <was5@cin.ufpe.br>
2022-09-27 19:35:51 -0300

    Task 2

---
+++ Task2/Main.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import stacker.rpn.lexer.Token;
+import stacker.rpn.lexer.TokenType;
+
+public class Main { 
+    public static void main (String [] args) {
+        try {
+            List<Token> tokenList = scan("Calc2.stk");
+            Stack<Float> stack = new Stack<Float>();
+            
+            for (int i = 0; i < tokenList.size(); i++) {
+                String element = tokenList.get(i).lexeme;
+
+                if (!"*+-/".contains(element)) {
+                    stack.add(Float.parseFloat(element));
+                } else {
+                    Float b = stack.pop();
+                    Float a = stack.pop();
+                    if (element.equals("+")) {
+                        stack.add(a + b);
+                    } else if (element.equals("-")) {
+                        stack.add(a - b);
+                    } else if (element.equals("*")) {
+                        stack.add(a * b);
+                    } else if (element.equals("/")) {
+                        stack.add(a / b); 
+                    }
+                }
+            }
+            
+            System.out.println("The result of the expression is " + stack.pop());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+	private static List<Token> scan(String sourceFilePath) throws IOException {
+        List<Token> tokens = new ArrayList<>();
+        File input = new File(sourceFilePath);
+        Scanner in = new Scanner(input);
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            if (line.matches("[0-9]+")) {
+                Token token = new Token(TokenType.PLUS, line);
+                tokens.add(token);
+            } else if (line.equals("+")) {
+               Token token = new Token(TokenType.PLUS, line);
+               tokens.add(token);
+            } else if (line.equals("-")) {
+                Token token = new Token(TokenType.PLUS, line);
+                tokens.add(token);
+            } else if (line.equals("*")) {
+               Token token = new Token(TokenType.PLUS, line);
+               tokens.add(token);
+            } else if (line.equals("/")) {
+                Token token = new Token(TokenType.PLUS, line);
+                tokens.add(token);
+            } else {
+                System.err.println("Error: Unexpected character: " + line);
+                System.exit(1);
+            }
+        }
+        in.close();
+        return tokens;
+	}
+}
